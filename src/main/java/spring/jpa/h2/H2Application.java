f22d0d61f7c4dd5ab2f7018db2c428a88323d586
administrator <masoudider@yahoo.com>
2019-10-26 02:27:47 +0330

    project is done

--- src/main/java/spring/jpa/h2/H2Application.java
+++ src/main/java/spring/jpa/h2/H2Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class H2Application {
+public class  H2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(H2Application.class, args);
